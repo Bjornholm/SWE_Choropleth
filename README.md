eb7493a1e76d81bdb6d4db43991f3c5b7779943c
Björn Holm <bjornholm88@gmail.com>
2022-07-18 19:43:51 +0200

    add slider to change shown year

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Testing Choropleth maps
 
 click_that_hood/public/data/sweden-counties.geojson
+
 https://www.scb.se/hitta-statistik/statistik-efter-amne/befolkning/befolkningens-sammansattning/befolkningsstatistik/
--- Swe_choro.py
+++ Swe_choro.py
@@ -10,8 +10,9 @@ import json
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
-pio.renderers.default='chromium'
+from dash import Dash, dcc, html, Input, Output
 
+pio.renderers.default='chromium'
 
 lan = json.load(open('swe_geo.json', 'r'))
 
@@ -19,21 +20,42 @@ kommun_pop = pd.read_csv('lan_1749-2020.csv',
                          sep = ',',
                          thousands=' ')
 
-#kommun_pop = kommun_pop.rename(columns = {'Kommun':'landskap'})
-#lan['features'][0]['properties']['name']
-# d = 1
-# for i in lan['features']:
-#     print(i['properties']['landskap'] + " {}".format(d))
-#     d = d+ 1
+year_columns = kommun_pop.columns[1:]
+#mapping years with data to linear index
+year_dict = {}
+i=1
+for y in year_columns:
+    year_dict.update({i :y })
+    i +=1
 
-plt = px.choropleth(kommun_pop, 
+#Dash interactive elements 
+#see https://plotly.com/python/mapbox-county-choropleth/
+
+app = Dash(__name__)
+app.layout = html.Div([
+    html.H4('Population density per region Sweden'),
+    html.P("Choose a year:"),
+#TODO rotate text on slider, update on value change, not mouse release
+    dcc.Slider(0,55,
+               step=None,
+               marks = year_dict,
+               value=1,id="year"),
+    dcc.Graph(id="graph"),
+])
+
+@app.callback(
+    Output("graph", "figure"), 
+    Input("year", "value"))
+def display_choropleth(year):
+
+    fig = px.choropleth(kommun_pop, 
                     geojson= lan,
                     locations= 'Län',
                     featureidkey='properties.name',
-                    color = '2020',
+                    color = year_dict[year],
                     hover_name = 'Län',
-                    hover_data = ['2020'],
+                    hover_data = [year_dict[year]],
                     title = 'Sverige population per län')
-
-plt.update_geos(fitbounds="locations", visible=False)
-plt.show()
+    fig.update_geos(fitbounds="locations", visible=True)
+    return fig
+app.run_server(debug=True)
